9cacafda64b23bc2502e929409a1ee4c7897ef90
Barla Fam <nd4zmoe@gmail.com>
2026-06-20 23:37:19 +0700

    Update app version from 17.6 to 18.0

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (PWA KASIR ENTERPRISE)
 // ==================================
 
-const APP_VERSION = '17.6'; 
+const APP_VERSION = '18.0'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
