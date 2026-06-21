201a40c73778d375dceef8aaf93fc66e6bc7b919
Barla Fam <nd4zmoe@gmail.com>
2026-06-21 08:42:07 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (PWA KASIR ENTERPRISE)
 // ==================================
 
-const APP_VERSION = '18.1'; 
+const APP_VERSION = '18.2'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
