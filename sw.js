a9e27fb7c448ffb757dd8f57dd292ae1692b5918
Barla Fam <nd4zmoe@gmail.com>
2026-06-25 14:27:31 +0700

    Update service worker version to 20.0

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (PWA KASIR ENTERPRISE)
 // ====================================
 
-const APP_VERSION = '19.9'; 
+const APP_VERSION = '20.0'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
