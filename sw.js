104c8bb54f296aeeb5037a88c72206631f9c62d3
Barla Fam <nd4zmoe@gmail.com>
2026-06-27 12:14:44 +0700

    Update app version to 1.1 in service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (PWA KASIR ENTERPRISE)
 // ====================================
 
-const APP_VERSION = '1.0'; 
+const APP_VERSION = '1.1'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
