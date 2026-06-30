39ba896a0119cb951f9bb9b8c66730304c2a40a9
Barla Fam <nd4zmoe@gmail.com>
2026-06-30 14:28:26 +0700

    Update app version to 1.8 in service worker

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // SERVICE WORKER (PWA KASIR ENTERPRISE)
 // ====================================
 
-const APP_VERSION = '1.7'; 
+const APP_VERSION = '1.8'; 
 const CACHE_CORE = 'core-v' + APP_VERSION; 
 const CACHE_DYNAMIC = 'dyn-v' + APP_VERSION;
 const CACHE_CDN = 'cdn-v1'; 
